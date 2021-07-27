9f69568037a958d0b00f2176f1ab23a1fd50e6ed
Justin Pflueger <jupflueg@microsoft.com>
2021-07-27 14:59:48 -0600

    add class library and unit test project

---
+++ src/GoogleFitOnFhir/Temporary.cs
@@ -0,0 +1,11 @@
+namespace GoogleFitOnFhir
+{
+    //TODO: remove when there is real code
+    public class Temporary
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
