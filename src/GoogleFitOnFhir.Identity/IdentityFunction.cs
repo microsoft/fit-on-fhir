a54524c12abc2288555c2192542810fa49f2a931
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-08-02 10:26:36 -0600

    Added syntax error to check if build fails

--- src/GoogleFitOnFhir.Identity/IdentityFunction.cs
+++ src/GoogleFitOnFhir.Identity/IdentityFunction.cs
@@ -17,7 +17,7 @@ namespace GoogleFitOnFhir.Identity
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.")
 
             string name = req.Query["name"];
 
