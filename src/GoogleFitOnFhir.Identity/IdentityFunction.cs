992199d8f86917add43a9eb9a0cdc282a6370593
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-08-04 14:29:56 -0600

    Added syntax error expected to throw warning.

--- src/GoogleFitOnFhir.Identity/IdentityFunction.cs
+++ src/GoogleFitOnFhir.Identity/IdentityFunction.cs
@@ -10,26 +10,26 @@ using Newtonsoft.Json;
 
 namespace GoogleFitOnFhir.Identity
 {
-    public static class IdentityFunction
-    {
-        [FunctionName("Identity")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+public static class IdentityFunction
+{
+[FunctionName("Identity")]
+public static async Task<IActionResult> Run(
+[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+ILogger log)
+{
+log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+dynamic data = JsonConvert.DeserializeObject(requestBody);
+name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+string responseMessage = string.IsNullOrEmpty(name)
+? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+: $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
-        }
-    }
+return new OkObjectResult(responseMessage);
+}
+}
 }
