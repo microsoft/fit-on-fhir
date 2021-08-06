4a2682cd77661e006ccd170ad9b4300063fe002e
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-08-06 10:11:22 -0600

    Fixed import ordering

--- src/GoogleFitOnFhir.Identity/IdentityFunction.cs
+++ src/GoogleFitOnFhir.Identity/IdentityFunction.cs
@@ -1,12 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System;
-using System.IO;
 
 namespace GoogleFitOnFhir.Identity
 {
