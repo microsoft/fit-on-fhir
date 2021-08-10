fb423e3006109ca6b8ee22a2a7254d257ebde667
Steve Gricci <52762109+stgricci@users.noreply.github.com>
2021-08-10 12:52:04 -0600

    Replacing editor config with Stylecop for consistency sake (#44)
    * Adding stylecop and removing editorconfig
    
    * Adding stylecop rulesets
    
    * Updating gitignore to actually ignore the vscode dir
    
    * Fixing StyleCop issues
    
    * Fixing readme capitalization

--- src/GoogleFitOnFhir/GoogleFitData.cs
+++ src/GoogleFitOnFhir/GoogleFitData.cs
@@ -8,25 +8,25 @@ namespace GoogleFitOnFhir
 {
     public class GoogleFitData
     {
-        protected FitnessService fitnessService;
+        private FitnessService fitnessService;
 
         public GoogleFitData(string accessToken)
         {
-            fitnessService = new FitnessService(new BaseClientService.Initializer()
+            this.fitnessService = new FitnessService(new BaseClientService.Initializer()
             {
-                HttpClientInitializer = GoogleCredential.FromAccessToken(accessToken)
+                HttpClientInitializer = GoogleCredential.FromAccessToken(accessToken),
             });
         }
 
         public ListDataSourcesResponse GetDataSourceList()
         {
-            var listRequest = new UsersResource.DataSourcesResource.ListRequest(fitnessService, "me");
+            var listRequest = new UsersResource.DataSourcesResource.ListRequest(this.fitnessService, "me");
             return listRequest.Execute();
         }
 
         public IomtDataset GetDataset(string dataStreamId, string datasetId)
         {
-            var datasourceRequest = new UsersResource.DataSourcesResource.DatasetsResource.GetRequest(fitnessService, "me", dataStreamId, datasetId);
+            var datasourceRequest = new UsersResource.DataSourcesResource.DatasetsResource.GetRequest(this.fitnessService, "me", dataStreamId, datasetId);
             return new IomtDataset(datasourceRequest.Execute());
         }
     }
