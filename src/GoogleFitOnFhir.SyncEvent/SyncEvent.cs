e9c295ce0213aa11098e4093a128dddb8d2fbd7d
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-08-16 13:51:35 -0600

    DI wip

--- src/GoogleFitOnFhir.SyncEvent/SyncEvent.cs
+++ src/GoogleFitOnFhir.SyncEvent/SyncEvent.cs
@@ -4,23 +4,34 @@ using Microsoft.Extensions.Logging;
 
 namespace GoogleFitOnFhir.SyncEvent
 {
+    [StorageAccount("AzureWebJobsStorage")]
     public static class SyncEvent
     {
         [FunctionName("SyncEvent")]
-        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        [return: Queue("myqueue-items")]
+        public static string Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
             // TODO: remove once using query in PR #62 - https://github.com/microsoft/googlefit-on-fhir/pull/62
-            string[,] usersArray = { { "A801C48320EC6E9A47EA2B844C9C7CC6", "2021-08-13T19:43:34.229Z" } };
-        }
+            string[,] usersArray =
+            {
+                {
+                    "A801C48320EC6E9A47EA2B844C9C7CC6",
+                    "2021-08-13T19:43:34.229Z",
+                },
+                {
+                    "A801C48320EC6E9A47EA2B844C9C7CC6",
+                    "2021-08-13T19:43:34.229Z",
+                },
+            };
+            foreach (string user in usersArray)
+            {
+                Console.WriteLine($"{user} ");
+                return user;
+            }
 
-        [FunctionName("QueueOutput")]
-        [return: Queue("myqueue-items")]
-        public static string QueueOutput([HttpTrigger] dynamic input,  ILogger log)
-        {
-            log.LogInformation($"C# function processed: {input.Text}");
-            return input.Text;
+            return string.Empty;
         }
     }
 }
