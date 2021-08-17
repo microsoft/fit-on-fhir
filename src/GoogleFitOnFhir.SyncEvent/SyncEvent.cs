b43f50a01ce074f52652aa6141c1ef594d2133f2
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-08-17 13:16:50 -0600

    Deduced that foreach loop is causing error

--- src/GoogleFitOnFhir.SyncEvent/SyncEvent.cs
+++ src/GoogleFitOnFhir.SyncEvent/SyncEvent.cs
@@ -6,27 +6,26 @@ using Microsoft.Extensions.Logging;
 
 namespace GoogleFitOnFhir.SyncEvent
 {
-    [StorageAccount("AzureWebJobsStorage")]
     public class SyncEvent
     {
         [FunctionName("SyncEvent")]
-        [return: Queue("publish-data")]
-        public static string Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ICollector<string> queueService, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, [Queue("publish-data", Connection = "AzureWebJobsStorage")] ICollector<string> queueService, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
             // TODO: remove once using query in PR #62 - https://github.com/microsoft/googlefit-on-fhir/pull/62
             string[] usersArray = { "A801C48320EC6E9A47EA2B844C9C7CC6", "A801C48320EC6E9A47EA2B844C9C7CC6" };
 
-            foreach (string user in usersArray)
-            {
-                Console.WriteLine($"{user} ");
-                var message = new QueueMessage();
-                message.UserId = user;
-                queueService.Add(queueService + "(step 1)");
-            }
+            // foreach (string userId in usersArray)
+            // {
+            //     Console.WriteLine($"{userId} ");
+            var message = new QueueMessage();
+            message.UserId = usersArray[0];
+            queueService.Add(message + "(step 1)");
+            Console.WriteLine(message.UserId);
 
-            return string.Empty;
+            // }
+            // return string.Empty;
         }
     }
 }
