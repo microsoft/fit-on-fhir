f42f56b9c1f566ca6af5e4627e12464ac602b584
Jason Held <jasonheld@microsoft.com>
2021-08-18 16:10:58 -0600

    Add dependency injection for SyncEvent, and pull users from storage table

--- src/GoogleFitOnFhir.SyncEvent/SyncEvent.cs
+++ src/GoogleFitOnFhir.SyncEvent/SyncEvent.cs
@@ -2,30 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using GoogleFitOnFhir.Models;
+using GoogleFitOnFhir.Repositories;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace GoogleFitOnFhir.SyncEvent
 {
     public class SyncEvent
     {
+        private readonly IUsersTableRepository usersTableRepository;
+
+        public SyncEvent(IUsersTableRepository usersTableRepository)
+        {
+            this.usersTableRepository = usersTableRepository;
+        }
+
         [FunctionName("SyncEvent")]
         public static void Run([TimerTrigger("0 */1 * * *")] TimerInfo myTimer, [Queue("publish-data", Connection = "AzureWebJobsStorage")] ICollector<string> queueService, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-            // TODO: Use GetAll() instead of hardcoded array (https://github.com/microsoft/googlefit-on-fhir/pull/62/files#diff-12ef371094ae0c16b4de28ded773034d72e48b3e54c11409ad9be5dbda4aa873R28)
-            var users = new List<string>()
-                { "A801C48320EC6E9A47EA2B844C9C7CC6", "DSFKJ39234LASKDFJNL349SDLFKSDF" };
+            IEnumerable<User> users = this.usersTableRepository.GetAll();
 
-            users.ForEach(userId =>
+            foreach (User user in users)
             {
-                var message = new QueueMessage
+                queueService.Add(JsonConvert.SerializeObject(new QueueMessage
                 {
-                    UserId = userId,
-                };
-                queueService.Add(message.ToString());
-            });
+                    UserId = user.Id,
+                }));
+            }
         }
     }
 }
