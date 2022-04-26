3745e38af80e94f57330a513664165bcb7835a4e
cgilbert76 <cgilbert76@users.noreply.github.com>
2022-04-26 18:33:40 -0400

    tests (#108)
    * RoutingServiceTests, IdentityDependencyInjectionExtensionsTests, and GoogleFitResponsibilityHandlerTests refactoring
    
    Co-authored-by: cgilbert76 <v-colgilbert@microsoft.com>

---
+++ src/GoogleFitOnFhir.UnitTests/Mocks/MockBaseResponsibilityHandler.cs
@@ -0,0 +1,26 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using GoogleFitOnFhir.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Health.Common.Handler;
+
+namespace GoogleFitOnFhir.UnitTests.Mocks
+{
+    internal class MockBaseResponsibilityHandler : IResponsibilityHandler<RoutingRequest, Task<IActionResult>>
+    {
+        public MockBaseResponsibilityHandler()
+        {
+        }
+
+        public static IResponsibilityHandler<RoutingRequest, Task<IActionResult>> Instance { get; } = new MockBaseResponsibilityHandler();
+
+        public Task<IActionResult> Evaluate(RoutingRequest request)
+        {
+            return Task.Run<IActionResult>(() => new OkResult());
+        }
+    }
+}
