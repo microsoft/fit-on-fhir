9c915157bc82565c9ea01c7ee58bc95eeea04071
cgilbert76 <cgilbert76@users.noreply.github.com>
2022-08-18 21:05:07 -0400

    Authorize function returns JSON object (#151)
    * add AuthorizeResponseData as return type for authorize request
    
    Co-authored-by: cgilbert76 <v-colgilbert@microsoft.com>

---
+++ src/Common/FitOnFhir.Common/Models/AuthorizeResponseData.cs
@@ -0,0 +1,31 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Health.FitOnFhir.Common.Models
+{
+    public class AuthorizeResponseData
+    {
+        [JsonConstructor]
+        public AuthorizeResponseData()
+        {
+        }
+
+        public AuthorizeResponseData(string authUrl, DateTimeOffset expiresAt)
+        {
+            AuthUrl = authUrl;
+            ExpiresAt = expiresAt;
+        }
+
+        [JsonProperty("authUrl")]
+        [JsonRequired]
+        public string AuthUrl { get; set; }
+
+        [JsonProperty("expiresAt")]
+        [JsonRequired]
+        public DateTimeOffset ExpiresAt { get; set; }
+    }
+}
